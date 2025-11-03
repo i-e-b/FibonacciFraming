98faeeefff6fb40775fde697b9cd51189f18d698
Iain Ballard <iain.ballard@gmail.com>
2025-11-03 13:13:56 +0000

    project shell

---
+++ FibonacciFraming/FibFramingTests/BasicTests.cs
@@ -0,0 +1,16 @@
+﻿using FibonacciFraming;
+using NUnit.Framework;
+
+namespace FibFramingTests;
+
+[TestFixture]
+public class BasicTests
+{
+    [Test]
+    public void can_accept_a_byte_array()
+    {
+        var result = Transcoder.ToSignal("Hello, world!"u8.ToArray());
+
+        Assert.That(result, Is.Not.Null);
+    }
+}
---
+++ FibonacciFraming/FibonacciFraming/Transcoder.cs
@@ -0,0 +1,17 @@
+﻿namespace FibonacciFraming;
+
+/// <summary>
+/// Transcoder to convert between byte data and transmission signals
+/// </summary>
+public class Transcoder
+{
+
+    /// <summary>
+    /// Convert a byte array into a framed transmission signal.
+    /// The result is padded with leading zeroes.
+    /// </summary>
+    public static byte[] ToSignal(byte[] rawData)
+    {
+        throw new NotImplementedException();
+    }
+}
