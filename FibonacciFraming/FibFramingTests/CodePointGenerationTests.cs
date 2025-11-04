8601c8b19d4f69e5325febc13a462023b58f5271
Iain Ballard <iain.ballard@gmail.com>
2025-11-04 13:02:17 +0000

    initial test of priority ordering

--- FibonacciFraming/FibFramingTests/CodePointGenerationTests.cs
+++ FibonacciFraming/FibFramingTests/CodePointGenerationTests.cs
@@ -83,24 +83,20 @@ public class CodePointGenerationTests
     [Test(Description = "This generates the main look-up tables for encode/decode")]
     public void fib_coding_mapping_tables()
     {
-        var priorityMap = new int[256]; // Order the byte values. Shortest codes first
-
-        for (int i = 0; i < 256; i++)
-        {
-            priorityMap[i] = i; // No priority
-        }
-
         var bits    = new int[256]; // bit patterns for byte values
         var lengths = new int[256]; // pattern lengths for bit patterns
 
+        var priorityMap = BuildPriorityMap();
 
         const int searchRange = 500;
 
         var backMap      = new int[searchRange];
         var codePoint    = 0;
         var maxCodePoint = 0;
-        for (var input = 0; input < searchRange; input++)
+        for (var input = 0; input < searchRange; input++) // For each Fibonacci code in the search range
         {
+
+            // Output the cod
             var data = new MemoryStream();
             var src  = new BitwiseStreamWrapper(data, 0);
             FibonacciEncoder.FibonacciEncodeOne(input, src);
@@ -113,6 +109,7 @@ public class CodePointGenerationTests
             var runningOne  = 0;
             var ok          = true;
 
+            // Check if the code is acceptable for our constraints
             while (src.TryReadBit(out var b))
             {
                 if (b == 1)
@@ -133,6 +130,7 @@ public class CodePointGenerationTests
                 if (runningOne > 1) break;
             }
 
+            // If acceptable, place in the mapping tables (smaller codes in higher priority slots)
             if (ok)
             {
                 FibonacciEncoder.FibonacciEncodeInt(input, out var bitPattern, out var length);
@@ -145,13 +143,15 @@ public class CodePointGenerationTests
 
                 codePoint++;
             }
+            // If not acceptable, mark it as unused in the backmap table
             else
             {
                 backMap[input] = -1;
             }
 
-            src.Rewind();
 
+            // Check that the decoder reads the code correctly
+            src.Rewind();
             var result = FibonacciEncoder.FibonacciDecodeOne(src);
 
             Assert.That(result, Is.EqualTo(input));
@@ -161,6 +161,11 @@ public class CodePointGenerationTests
 
         Assert.That(codePoint, Is.EqualTo(256), "Must fill all byte value slots");
 
+        WriteGeneratedCode(bits, lengths, maxCodePoint, backMap);
+    }
+
+    private static void WriteGeneratedCode(int[] bits, int[] lengths, int maxCodePoint, int[] backMap)
+    {
         var bitsArray    = new StringBuilder();
         var lengthsArray = new StringBuilder();
         var backmapArray = new StringBuilder();
@@ -197,5 +202,31 @@ public class CodePointGenerationTests
         Console.WriteLine(lengthsArray);
         Console.WriteLine(backmapArray);
         Console.WriteLine("\n}");
+    }
+
+    private static int[] BuildPriorityMap()
+    {
+        var priorityBytes  = "__ etaoinsrhldcumfgpwybvkjqxz"u8.ToArray();
+        priorityBytes[0] = 0x00;
+        priorityBytes[1] = 0xFF;
+        var remainingBytes = new Dictionary<int, bool>();
+
+        for (int i = 0; i < 256; i++) remainingBytes.Add(i, true);
+
+        var priorityMap = new int[256]; // Order the byte values. Shortest codes first
+
+        var pos = 0;
+        foreach (var b in priorityBytes)
+        {
+            priorityMap[pos++] = b;
+            remainingBytes[b] = false;
+        }
+
+        foreach (var b in remainingBytes)
+        {
+            if (b.Value) priorityMap[pos++] = b.Key;
+        }
+
+        return priorityMap;
     }
 }
